a7d26f29d836f3e11ac765923027996b1f8402a5
nick22morris <nick88morris22@gmail.com>
2025-01-09 09:18:21 -0600

    Initial Commit

---
+++ Defensio/DefensioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefensioApp.swift
+//  Defensio
+//
+//  Created by Nick Morris on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DefensioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
